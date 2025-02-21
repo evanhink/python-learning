41476f5ab09f8ed5ea92c1f3e513dd218da9ded4
Evan Hinkley <evanarrel@icloud.com>
2025-02-21 07:52:20 -0700

    Update area_calculator.py
    grammar

--- area_calculator.py
+++ area_calculator.py
@@ -1,14 +1,14 @@
 print("This program is for calculating the area of a 2 dimensional rectangle.")
-print("The area of a rectangle is calculated with Area = Length * Width.")
+print("The area of a rectangle is calculated with: area = length * width.\n")
 
 #take the user's input to store a variable named length and width, then convert it
 #to floating point numbers with the float() function
 
 length = float(input("Enter length in centimeters: "))
-width = float(input("Enter heigh in centimeterst: "))
+width = float(input("Enter height in centimeters: "))
 
 
 #taking the user's input of length and width, multiplying them, then printing
 #the product with an f-string
 area = (length * width)
-print(f"The area of your rectangle is {area}c cm².")
+print(f"The area of your rectangle is {area} cm².")
